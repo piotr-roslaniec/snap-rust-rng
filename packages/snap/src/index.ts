34c27d9c24e235393a002c2f53d819e96d5c2f93
Michał Konopka <konopek68@gmail.com>
2022-11-15 21:31:21 +0100

    add rollup => it build in SES

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,31 +1,23 @@
 import { OnRpcRequestHandler } from '@metamask/snap-types';
-import {
-  InitOutput,
-  // add_random,
-  add_random_with_seed
-} from 'wasm-bundler';
-import { getRandomBytes } from './random';
 
-import { initializeWasm } from './wasm';
+import initWasm from '../../../rust-rng/dist/index';
 
-let wasm: InitOutput;
+let wasm;
+
 
 export const onRpcRequest: OnRpcRequestHandler = async ({
   origin,
   request,
 }) => {
   if (!wasm) {
-    wasm = await initializeWasm();
+    wasm = await initWasm();
   }
 
   console.log({ request });
 
   switch (request.method) {
-    // Uncomment in order to reproduce the original error
-    // case 'add_random':
-    //   return add_random(request.params[0]);
-    case 'add_random_with_seed':
-      return add_random_with_seed(request.params[0], getRandomBytes());
+    case 'add':
+      return wasm.add(2, 2);
     default:
       throw new Error('Method not found.');
   }
